533aee8ca634fa152c0bc71e79f1c98b7ed99fb5
husada20 <husadahutasoit@gmail.com>
2023-09-01 21:43:35 +0700

    initial commit

--- src/MathBasic.js
+++ src/MathBasic.js
@@ -1,14 +1,24 @@
 const MathBasic = {
-    add: function add() {
+    add: (... args) => {
+        if(args.length  !== 2) {
+            throw new Error('fungsi hanya menerima dua parameter')
+        }
+
+       const [a, b] = args
+
+        if(typeof a !== 'number' || typeof b !== 'number') {
+            throw new Error('fungsi hanya menerima parameter number')
+        }
+
+        return a + b;
+    },
+    subtract: () => {
    
     },
-    subtract: function subtract() {
+    multiply: () => {
    
     },
-    multiply: function multiply() {
-   
-    },
-    divide: function divide() {
+    divide: () => {
    
     },
   };
